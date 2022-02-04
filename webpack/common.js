3709b31b41bb1110886cc254af824fdeb2b16883
Hunter Keca <hunter@heysparkbox.com>
2022-02-04 06:19:54 -0600

    chore: setup webpack & typescript
    - Add tsconfig for initial typescript configuration
    - Test dev webpack config to get started

---
+++ webpack/common.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+exports.common = {
+  entry: './src/app.ts',
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'dist')
+  },
+};
